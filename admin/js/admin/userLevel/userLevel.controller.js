736525cac7298fabc44eedd50439f68047afdb98
Tomwang@lagou.com <Tomwang@lagou.com>
2018-03-04 23:24:42 +0800

    备份

--- admin/js/admin/userLevel/userLevel.controller.js
+++ admin/js/admin/userLevel/userLevel.controller.js
@@ -69,6 +69,7 @@
                 ariaDescribedBy: 'modal-body',
                 templateUrl: '/views/admin/userLevel/'+templateName+'.html',
                 controller: controllerName,
+                size: 'lg',
                 resolve: {
                     item: item
                 }
